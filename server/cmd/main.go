595e88f4ea5426bc805a69360d5af629dfca473c
Kevin Ruhl <kevin.ruhl92@gmail.com>
2024-06-25 00:46:36 -0300

    registers ws handler

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"log"
 	"server/db"
+	"server/internal/ws"
+	"server/router"
 )
 
 func main() {
-	_, err := db.NewDatabase()
+	dbConn, err := db.NewDatabase()
+
 	if err != nil {
-		log.Fatalf("Could not initialize database connection: %s", err)
+		log.Fatalf("could not initialize database connection: %s", err)
 	}
+
+	hub := ws.NewHub()
+	wsHandler := ws.NewHandler(hub)
+
+	router.InitRouter(wsHandler)
+	router.Start("0.0.0.0:8080")
+
 }
